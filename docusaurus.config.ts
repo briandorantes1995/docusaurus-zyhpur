5aea699e93e9ff42c30b75785e583a35a14f59c4
briandorantes <briandorantes@hotmail.com>
2026-03-20 23:02:33 -0600

    correcion de datalab

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,12 +18,12 @@ const config: Config = {
   url: 'https://briandorantes1995.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/zyphurdocuments/',
+  baseUrl: '/docusaurus-zyhpur/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'briandorantes1995', // Usually your GitHub org/user name.
-  projectName: 'zyphurdocuments', // Usually your repo name.
+  projectName: 'docusaurus-zyhpur', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
